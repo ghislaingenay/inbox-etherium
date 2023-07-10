6b320eb048332c6f3429ad5f942a346c78a300e1
ghislaingenay <ghislain.gny@gmail.com>
2023-07-10 12:31:46 +0700

    Recover data from Inbox from compiler

--- compile.js
+++ compile.js
@@ -5,4 +5,4 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
-solc.compile(source, 1); // 1 is the number of contracts we want to compile
+module.exports = solc.compile(source, 1).contracts[":Inbox"]; // 1 is the number of contracts we want to compile
